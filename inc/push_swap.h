86ff2db9bea7e169fb6e06187cef243006246751
Teemu Spoof <tspoof@student.hive.fi>
2023-02-13 16:52:35 +0200

    changed position to be unsigned int

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 15:27:34 by tspoof            #+#    #+#             */
-/*   Updated: 2023/02/13 13:41:17 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/02/13 16:49:03 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 typedef struct s_stack
 {
 	int				content;
-	int				pos;
+	unsigned int	pos;
 	struct s_stack	*next;
 	struct s_stack	*prev;
 }					t_stack;
@@ -76,5 +76,11 @@ void	ft_stack_rotate(t_stack **head);
  * @param head
  */
 void	ft_stack_reverse_rotate(t_stack **head);
+/**
+ * @brief Counts number of nodes.
+ *
+ * @return size_t
+ */
+size_t	ft_stack_len(t_stack *head);
 
 #endif
