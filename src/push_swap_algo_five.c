4c27327f2ed1324eb78fb7b686bdbfe5f3fb0ddf
Teemu Spoof <tspoof@student.hive.fi>
2023-02-15 20:49:28 +0200

    started algo_five

--- src/push_swap_algo_five.c
+++ src/push_swap_algo_five.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:53:33 by tspoof            #+#    #+#             */
-/*   Updated: 2023/02/15 19:59:16 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/02/15 20:38:40 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,10 @@ void	push_swap_five(t_stack **stack_a, t_stack **stack_b)
 {
 	(void) stack_a;
 	(void) stack_b;
+
+	while ((*stack_a)->next != NULL)
+	{
+		if ((*stack_a)->pos == 0 || (*stack_a)->pos == 1)
+
+	}
 }
